61948b32ace73513a82097036277544a6557c176
chesteric31 <eric.binard.182@gmail.com>
2020-10-31 15:15:01 +0100

    initial commit

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,30 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'armor-category',
+        loadChildren: () => import('./armor-category/armor-category.module').then(m => m.JsaintseiyaArmorCategoryModule),
+      },
+      {
+        path: 'strength',
+        loadChildren: () => import('./strength/strength.module').then(m => m.JsaintseiyaStrengthModule),
+      },
+      {
+        path: 'armor',
+        loadChildren: () => import('./armor/armor.module').then(m => m.JsaintseiyaArmorModule),
+      },
+      {
+        path: 'armor-strength',
+        loadChildren: () => import('./armor-strength/armor-strength.module').then(m => m.JsaintseiyaArmorStrengthModule),
+      },
+      {
+        path: 'armor-version',
+        loadChildren: () => import('./armor-version/armor-version.module').then(m => m.JsaintseiyaArmorVersionModule),
+      },
+      {
+        path: 'armor-version-attack',
+        loadChildren: () => import('./armor-version-attack/armor-version-attack.module').then(m => m.JsaintseiyaArmorVersionAttackModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
